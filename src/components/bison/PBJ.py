0e6f97bf75a968c821376664eef93cc84d5ba111
Jim Hourihan <jimh@Ultraseven.local>
2022-07-24 12:28:32 -0700

    Fix bison PBJ

--- src/components/bison/PBJ.py
+++ src/components/bison/PBJ.py
@@ -9,7 +9,7 @@ URL          = f"https://ftp.gnu.org/gnu/bison/{ARCHIVE}"
 ORIGIN_TREE  = "pbj"
 TARGET       = ["$BUILD_ROOT/bin/bison",
                 "$BUILD_ROOT/bin/yacc",
-                "$BUILD_ROOT/lib/liby.a"
+                "$BUILD_ROOT/lib/liby.a",
                 "$BUILD_ROOT/share/info/bison.info",
                 "$BUILD_ROOT/share/bison"]
 
